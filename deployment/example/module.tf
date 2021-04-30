a5be0083b37f2635ab299627bcd247fbdca9f0c2
Your Name <you@example.com>
2021-04-30 23:24:50 +0000

    latest

--- deployment/example/module.tf
+++ deployment/example/module.tf
@@ -2,7 +2,7 @@ module "gke_cluster" {
     source  = "../../"
     cluster_name = "fuchicorp-cluster"
     google_region = "us-central1-c"
-    google_project_id =  "perceptive-ivy-308201"
+    google_project_id =  "flash-sol-311601"
     cluster_node_count = "2"
     cluster_version = "1.15"
     google_credentials = "./fuchicorp-service-account.json" # service account
