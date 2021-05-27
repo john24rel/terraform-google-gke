9fb9f19ac1a35b42250c1affa745ccce724655a0
JOHN OSHIKOYA <72477831+john24rel@users.noreply.github.com>
2021-05-27 15:41:40 -0500

    Update module.tf

--- deployment/example/module.tf
+++ deployment/example/module.tf
@@ -1,10 +1,10 @@
 module "gke_cluster" {
     source  = "../../"
-    cluster_name = "fuchicorp-cluster"
-    google_region = "us-central1-c"
-    google_project_id =  "flash-sol-311601"
-    cluster_node_count = "2"
-    cluster_version = "1.15"
+    cluster_name = "fuchicorp-cluster"  # name of your cluster
+    google_region = "us-central1-c"  # region you wanna deploy to
+    google_project_id =  "flash-sol-311601"  #your project id
+    cluster_node_count = "2" 
+    cluster_version = "1.17"
     google_credentials = "./fuchicorp-service-account.json" # service account
     image_type        = "COS"
     disk_size_in_gb   = "10"
