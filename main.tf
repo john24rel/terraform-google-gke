376fc7e17e0bb91b326065600326e93253ff0d4f
aziz_shoko <azizbek.shokosimov@gmail.com>
2022-05-20 12:23:38 -0500

    added spot instaces and on demand instance

--- main.tf
+++ main.tf
@@ -2,9 +2,8 @@ data "google_container_engine_versions" "cluster_version" {
   location       = "${var.google_region}"
   version_prefix = "${var.cluster_version}"
   project        = "${var.google_project_id}"
-
-  
 }
+
 output "cluster_version" {
   value = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
 }
@@ -14,7 +13,15 @@ provider "google" {
   project     = "${var.google_project_id}"
 }
 
+provider "google-beta" {
+  credentials = "${file("${var.google_credentials}")}" #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
+  project     = "${var.google_project_id}"
+  version     = "4.4.0"
+}
+
+
 resource "google_container_cluster" "create" {
+  provider           = "google-beta"
   min_master_version = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
   node_version       = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
   name               = "${var.cluster_name}"
@@ -22,28 +29,52 @@ resource "google_container_cluster" "create" {
   subnetwork         = "${var.subnetwork}"
   location           = "${var.google_region}"
   project            = "${var.google_project_id}"
- 
- node_pool {
+
+  node_pool {
     name               = "${var.node_pool}"
     initial_node_count = "${var.cluster_node_count}"
+
     management {
-    auto_repair        = "${var.auto_repair}"
-    auto_upgrade       = "${var.auto_upgrade}"
+      auto_repair  = "${var.auto_repair}"
+      auto_upgrade = "${var.auto_upgrade}"
     }
+
     autoscaling {
-    min_node_count     = "${var.min_desired_count}"
-    max_node_count     = "${var.max_desired_count}"
+      min_node_count = "${var.min_desired_count}"
+      max_node_count = "${var.max_desired_count}"
     }
- node_config {
-    image_type       = "${var.image_type}"
-    disk_size_gb     = "${var.disk_size_in_gb}"
-    preemptible      = "${var.preemptible_nodes}"
-    machine_type     = "${var.machine_type}"
-    labels           = "${var.labels}"
+
+    node_config {
+      image_type   = "${var.image_type}"
+      disk_size_gb = "${var.disk_size_in_gb}"
+      preemptible  = "${var.preemptible_nodes}"
+      machine_type = "${var.machine_type}"
+      labels       = "${var.labels}"
+      spot         = "${var.spot_instance}"
+
+      metadata = {
+        ssh-keys                 = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+        disable-legacy-endpoints = "true"
+      }
+    }
+  }
+}
+
+resource "google_container_node_pool" "on_demand_nodes" {
+  name       = "on-demand-node"
+  cluster    = google_container_cluster.create.id
+  node_count = "${var.on_demand_node_count}"
+
+  node_config {
+    image_type   = "${var.image_type}"
+    disk_size_gb = "${var.disk_size_in_gb}"
+    preemptible  = "${var.preemptible_nodes}"
+    machine_type = "${var.machine_type}"
+    labels       = "${var.labels}"
+
     metadata = {
-      ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+      ssh-keys                 = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
       disable-legacy-endpoints = "true"
-    }
     }
   }
 }
